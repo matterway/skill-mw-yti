68286c4e936d6d4af1c4758871710d3cd408d424
karim-sh <ksharba@matterway.io>
2024-06-07 14:09:33 +0200

    extract partner data from yeti

--- src/steps/@start.tsx
+++ src/steps/@start.tsx
@@ -1,21 +1,14 @@
-import {Context, showMessage} from '@matterway/sdk';
-import {t} from 'i18next';
-import manifest from 'manifest.json';
+import {Context} from '@matterway/sdk';
 import {successStep} from 'steps/@success';
+import {gatherPartnerDataStep} from './gatherPartnerDataStep';
 
 // DO NOT add your automation in this step. Rather, create another step from
 // `_template.tsx`, and call them here
 
 export async function startStep(ctx: Context) {
   console.log('step: startStep');
-
-  await showMessage(ctx, {
-    title: manifest.name,
-    description: manifest.description,
-    text: t('start.text'),
-    buttons: [{text: t('start.proceedButton'), value: 'ok'}],
-  });
-
+  const partnerData = await gatherPartnerDataStep(ctx);
+  console.log('partnerData', partnerData);
   // Make a new step from `_template.tsx` and change this line to point to it
   await successStep(ctx);
 }
